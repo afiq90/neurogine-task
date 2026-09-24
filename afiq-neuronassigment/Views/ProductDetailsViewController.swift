923e6ec7f4bb0c98e7d498cdd40ab4be5ccdd223
Afiq <mafiqh90@gmail.com>
2026-09-24 16:32:11 +0800

    add and configure imagesCollectionView data souce and delegates, add ProductDetailViewModel, handle the views based on state from viewModel.state, add retryButtonTapped for retry functionality

--- afiq-neuronassigment/Views/ProductDetailsViewController.swift
+++ afiq-neuronassigment/Views/ProductDetailsViewController.swift
@@ -21,18 +21,67 @@ class ProductDetailsViewController: UIViewController, UICollectionViewDelegate, 
     @IBOutlet weak var productStackView: UIStackView!
     
     var productID: Int!
+    private var viewModel: ProductDetailViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagesCollectionView.dataSource = self
+        imagesCollectionView.delegate = self
+        
+        viewModel = ProductDetailViewModel(productID: productID)
+        viewModel.onChange = { [weak self] in
+            self?.render()
+        }
+        
+        viewModel.load()
+    }
+    
+    private func render() {
+        switch viewModel.state {
+        case .loading:
+            errorView.isHidden = true
+            productStackView.isHidden = true
+            loadingView.isHidden = false
+            loadingView.startAnimating()
+        case .success
+            guard let product = viewModel.product else {return}
+            
+            loadingView.stopAnimating()
+            loadingView.isHidden = true
+            errorView.isHidden = true
+            productStackView.isHidden = false
+            
+            titleLabel.text = product.title
+            priceLabel.text = String(format: "$%.2f", product.price)
+            ratingLabel.text = String(format: "Rating: %.1f / 5", product.rating)
+            descriptionLabel.text = product.description
+            imagesCollectionView.reloadData()
+        case .error(let errMsg):
+            loadingView.stopAnimating()
+            loadingView.isHidden = true
+            productStackView.isHidden = true
+            errorLabel.text = errMsg
+            errorView.isHidden = false
+        }
+    }
+    
+    @IBAction func retryButtonTapped(_ sender: UIButton) {
+        print("retry button tapped on productdetailsviewcontroller")
+        viewModel.retry()
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        viewModel.product?.images.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductImageCell", for: indexPath) as! ProductImageCell
+        let imageURL = viewModel.product?.images[indexPath.row]
+        cell.configure(with: imageURL)
+        
+        return cell
     }
     
     
