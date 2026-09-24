1fad7c670fada4ef00a171959d3804f9cd7c8f83
Afiq <mafiqh90@gmail.com>
2026-09-24 14:04:21 +0800

    Initial Commit

---
+++ afiq-neuronassigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  afiq-neuronassigment
+//
+//  Created by Afiq Hamdan on 24/09/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ afiq-neuronassigmentTests/afiq_neuronassigmentTests.swift
@@ -0,0 +1,19 @@
+//
+//  afiq_neuronassigmentTests.swift
+//  afiq-neuronassigmentTests
+//
+//  Created by Afiq Hamdan on 24/09/2026.
+//
+
+import Testing
+@testable import afiq_neuronassigment
+
+struct afiq_neuronassigmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
