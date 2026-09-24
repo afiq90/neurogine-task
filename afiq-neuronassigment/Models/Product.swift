de2b73d54cfe83f9c69152caf2718703f30877f2
Afiq <mafiqh90@gmail.com>
2026-09-24 14:32:12 +0800

    use Decodable for Product model to transform  json from API  to swift object

--- afiq-neuronassigment/Models/Product.swift
+++ afiq-neuronassigment/Models/Product.swift
@@ -5,3 +5,21 @@
 //  Created by Afiq Hamdan on 24/09/2026.
 //
 
+import Foundation
+
+struct ProductPage: Decodable {
+    let products: [Product]
+    let total: Int
+    let skip: Int
+    let limit: Int
+}
+
+struct Product: Decodable {
+    let id: Int
+    let title: String
+    let description: String
+    let price: Double
+    let rating: Double
+    let thumbnail: String
+    let images: [String]
+}
