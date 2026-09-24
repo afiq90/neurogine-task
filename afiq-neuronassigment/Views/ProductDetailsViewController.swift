369c3de91a5a7342f6511d88415d34dc4008e1d3
Afiq <mafiqh90@gmail.com>
2026-09-24 15:46:07 +0800

    add ProductDetailsViewController

---
+++ afiq-neuronassigment/Views/ProductDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductDetailsViewController.swift
+//  afiq-neuronassigment
+//
+//  Created by Afiq Hamdan on 24/09/2026.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    var productID: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+
+}
